c6b5d43995e0f52d34f326662f5473a04ab1bce9
Helen Dikareva <elena.dikareva@devexpress.com>
2020-10-28 15:51:00 +0300

    customers

--- 5_client_code/client_functions/2_element-property.js
+++ 5_client_code/client_functions/2_element-property.js
@@ -4,15 +4,13 @@ fixture('Client Function demo')
     .page('https://www.w3schools.com/html/html_tables.asp');
 
 test('Get element property', async t => {
-    const customers = Selector('#customers');
+    const table = Selector('#customers');
 
     const getFnText = ClientFunction((row, col) => {
             return getTable().rows[row].cells[col].innerText;
         },
 
-        {
-            dependencies: { getTable: customers }
-        }
+        { dependencies: { getTable: table } }
     );
 
     await t.expect(getFnText(1, 1)).eql('Maria Anders');
